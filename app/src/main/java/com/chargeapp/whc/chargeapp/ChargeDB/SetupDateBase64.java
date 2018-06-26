986d9f69f3c16ef61ddda5d2aeaedd621ad1ded0
1709008NB01 <s>
2018-06-26 08:54:12 +0800

    HmacSHA256

--- app/src/main/java/com/chargeapp/whc/chargeapp/ChargeDB/SetupDateBase64.java
+++ app/src/main/java/com/chargeapp/whc/chargeapp/ChargeDB/SetupDateBase64.java
@@ -264,5 +264,12 @@ public class SetupDateBase64 extends AsyncTask<Object, Integer, String> {
         return Base64.encodeToString(bytes,2);
     }
 
+    public  String encodeSHA256(String data) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        return Base64.encodeToString(sha256_HMAC.doFinal(data.getBytes("UTF-8")),2);
+    }
+
 
 }
